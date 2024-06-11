50011a1e47ed6248c5a7854080de0cad82cbf0d2
ujjwalkumarjaiswal <153944485+ujjwalkumarjaiswal@users.noreply.github.com>
2024-06-11 21:40:23 +0530

    Add files via upload

---
+++ project3.js
@@ -0,0 +1,14 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const display = document.getElementById('display');
+    const buttons = Array.from(document.querySelectorAll('.btn'));
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            const value = this.dataset.value;
+
+            if (value === 'C') {
+                display.innerText = '';
+            } else if (value === '=') {
+                try {
+                    display.innerText = eval(display.innerText
